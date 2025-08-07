eb204bfc90ed5947661e11718fa37a2373f29810
dohee <doheeeq@gmail.com>
2025-08-07 10:44:27 +0900

    토이프로젝트: 로그인, 회원가입 html, 회원가입 css 작성

---
+++ src/main/java/com/spring/toyproject/routes/PageController.java
@@ -0,0 +1,32 @@
+package com.spring.toyproject.routes;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 페이지 전환 랜더링용 컨트롤러
+ * Thymeleaf 나 JSP 같은 뷰템플릿 페이지를 랜더링
+ */
+@Controller
+@Slf4j
+public class PageController {
+
+    // 홈으로 이동
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    // 로그인으로 이동
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    // 회원가입으로 이동
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
+}
