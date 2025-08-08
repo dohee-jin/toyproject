75d12d587481de026266ef255f9ea340df3c4070
dohee <doheeeq@gmail.com>
2025-08-08 11:49:00 +0900

    토이프로젝트: 프론트엔드(로그인 인증, 로컬스토리지 토근 저장, 로그아웃 처리)

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,6 +1,7 @@
 /**
  * 우리 앱의 진입점 js 파일
  */
+import {authService} from "./utils/auth.js";
 
 // 현재 페이지 확인 함수
 const getCurrentPage = () => {
@@ -19,8 +20,28 @@ const App = () => {
     // 외부 모듈 js 파일들을 로드
     // 현재 어떤 페이지에 진입했는지 알아야 그에 맞는 js를 가져올 수 있음
     const currentPage = getCurrentPage();
+
+    // 공통 이벤트 바인딩
+    const bindEvents = () => {
+        // 로그아웃 이벤트
+        document.addEventListener('click', e => {
+            if(e.target.closest('[data-action="logout"]')) {
+                e.preventDefault();
+                // 실제 로그아웃 처리 (localStorage에 있는 로그인 관련 데이터 삭제)
+                authService.logout();
+            }
+        })
+    }
+
     const init = async () => {
         try {
+
+            // header 인증 UI 업데이트
+            authService.updateHeaderUI();
+
+            // 앱의 공통 이벤트 바인딩
+            bindEvents();
+
             const module = await import(`./pages/${currentPage}.js`);
             // console.log(module);
 
--- src/main/resources/static/js/pages/login.js
+++ src/main/resources/static/js/pages/login.js
@@ -1,12 +1,14 @@
-import{apiService} from "../utils/api.js";
-import{utils} from "../utils/util.js";
+import { apiService } from '../utils/api.js';
+import { utils } from '../utils/util.js';
+import { authService } from '../utils/auth.js';
+
 
 // 로그인 관련 함수들의 모음
 const LoginPage = () => {
 
     const state = {
-        $form: null
-    }
+        $form: null,
+    };
 
     // 로그인 이벤트
     const handleLogin = async (e) => {
@@ -15,35 +17,35 @@ const LoginPage = () => {
         // 폼 입력값 읽기
         const formData = new FormData(state.$form);
         const payload = Object.fromEntries(formData.entries());
-        console.log(`entries:`, payload);
 
         try {
+            const response = await apiService.post('/api/auth/login', payload);
 
-            const response = await apiService.post('api/auth/login', payload);
+            // 전달받은 토큰을 브라우저 스토리지에 안전하게 저장
+            // localStorage    : 자동 로그인 1일 구현
+            // sessionStorage  : 브라우저를 닫을때까지 로그인 유지
+            authService.login(response.data.token, JSON.stringify(response.data.user));
+
             utils.showMessage(response.message, 'success');
-
+            utils.redirectTo('/', 1000);
         } catch (error) {
             utils.showMessage(error.message, 'error');
         }
-
-    }
+    };
 
     // 이벤트 걸기
     const bindEvents = () => {
         state.$form?.addEventListener('submit', handleLogin);
-    }
+    };
 
     // 초기화 함수
     const init = () => {
-
-        console.log("로그인 js가 로딩되었습니다.");
-
         state.$form = document.getElementById('loginForm');
-
         bindEvents();
-    }
+        console.log(`123`);
+    };
 
-    // return 으로 함수를 내보내면 public 한 함수가 됨
+    // public 함수
     return {
         init
     };
