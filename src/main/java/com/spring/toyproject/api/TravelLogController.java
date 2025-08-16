73c0c7477cf51d03950af4bafc8c6ec24bd51112
dohee-jin <doheeeq@gmail.com>
2025-08-17 02:13:34 +0900

    토이프로젝트: 여행일지 생성 api 생성, postmapping 여러 타입 요청 받을 때 처리방식

---
+++ src/main/java/com/spring/toyproject/api/TravelLogController.java
@@ -0,0 +1,47 @@
+package com.spring.toyproject.api;
+
+import com.spring.toyproject.domain.dto.common.ApiResponse;
+import com.spring.toyproject.domain.dto.request.TravelLogRequestDto;
+import com.spring.toyproject.service.TravelLogService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api/travel-logs")
+public class TravelLogController {
+
+    private final TravelLogService travelLogService;
+
+    /**
+     * 여행일지 생성 API
+     * POST /api/travel-logs
+     *
+     * consumes: 클라이언트가 보낸 데이터의 형태 (기본값: json)
+     * 보통 application/json 형태임
+     * 하나의 api에 타입이 다른 여러 요청을 보낼 때
+     * multipart/form-data 를 사용
+     * @RequestPart(name ="")
+     *
+     * produces: 서버가 응답할 때 보내는 데이터 (기본값: json)
+     */
+    @PostMapping/*(consumes = "multipart/form-data; boundary=")*/
+    public ResponseEntity<?> createTravelLog(
+            @RequestBody TravelLogRequestDto requestDto,
+            @RequestPart(name = "data") Long tripId,
+            @RequestPart(name = "image") List<MultipartFile> files,
+            @AuthenticationPrincipal String username
+    ) {
+        log.info("여행 일지 생성 API 호출 - 사용자: {}, 여행 ID{}", username, tripId);
+        travelLogService.createTravelLog(requestDto, tripId, username, files);
+        return ResponseEntity.ok()
+                .body(ApiResponse.success("여행일지가 성공적으로 생성되었습니다.", null));
+    }
+}
